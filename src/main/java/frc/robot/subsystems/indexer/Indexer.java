8233ebae0e689fd5db6b03de761297152a794339
WilsonPerson <255547456+WilsonPerson@users.noreply.github.com>
2026-02-21 12:01:46 -0500

    feat(indexer): create indexer implementation for TalonFX

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -1,7 +1,36 @@
 package frc.robot.subsystems.indexer;
 
+import java.util.logging.Logger;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.indexer.IndexerIO.IndexerIOInputs;
 
 public class Indexer extends SubsystemBase{
-    
+    private final IndexerIO io;
+    private IndexerIOInputsAutoLogged inputs = new IndexerIOInputsAutoLogged();
+
+    public Indexer(IndexerIO io){
+        this.io = io;
+    }
+
+
+    @Override 
+    public void periodic(){
+        io.updateInputs(inputs);
+    }
+
+    public Command runIndexerOpenLoopCommand(double input){
+        return Commands.run(() -> io.setIndexerOpenLoop(input * IndexerConstants.MOTOR_SPEED_PERCENTAGE), this);
+    }
+
+    public Command runIndexerVelocityCommand(double velocity){
+        return Commands.run(() -> io.setIndexerVelocity(velocity), this);
+
+    }
+
+    public Command runIndexerVoltageCommand(double volts){
+        return Commands.run(() -> io.setIndexerVoltage(volts), this);
+    }
 }
--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -1,5 +1,29 @@
 package frc.robot.subsystems.indexer;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.signals.InvertedValue;
+
 public class IndexerConstants {
-    
+
+    public static final double MOTOR_SPEED_PERCENTAGE = 0.3;
+
+    public static final class TalonFXConstants {
+        public static final int MOTOR_ID = 17;
+        public static final double GEAR_RATIO = (64 / 16) * 3; // 3:1 gearbox with a 16:64 belt reduction
+        public static final Slot0Configs MOTOR_GAINS = new Slot0Configs()
+                .withKP(0.0)
+                .withKD(0.0)
+                .withKS(0.0)
+                .withKV(0.0);
+
+        public static final CurrentLimitsConfigs MOTOR_CURRENT_LIMITS = new CurrentLimitsConfigs()
+                .withSupplyCurrentLowerLimit(30.0);
+
+        public static final MotorOutputConfigs MOTOR_OUTPUT_CONFIGS = new MotorOutputConfigs()
+                .withInverted(InvertedValue.CounterClockwise_Positive);
+
+    }
+
 }
