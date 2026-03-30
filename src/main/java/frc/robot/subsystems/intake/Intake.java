4129dce52860936ed0ab8a1bcc52abef6453cbe6
WilsonPerson <255547456+WilsonPerson@users.noreply.github.com>
2026-03-30 19:06:54 -0400

    feat(intake): get intake to work

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -5,35 +5,32 @@ import frc.robot.subsystems.intake.IntakeConstants.IntakeConsts;
 import frc.robot.util.LoggedTunableNumber;
 
 public class Intake extends SubsystemBase {
-    private final IntakeIO io;
-    private final IntakeIOAutoLogged inputs = new IntakeIOAutoLogged();
+  //private final IntakeIO io;
+ // private final IntakeIOAutoLogged inputs = new IntakeIOAutoLogged();
 
-    private intakeState statusGoal = intakeState.OFF;
+  private intakeState statusGoal = intakeState.OFF;
 
-    private final LoggedTunableNumber intakekP = new LoggedTunableNumber(
-            "Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kP);
-    private final LoggedTunableNumber intakekD = new LoggedTunableNumber(
-            "Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kD);
-    private final LoggedTunableNumber intakekS = new LoggedTunableNumber(
-            "Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kS);
-    private final LoggedTunableNumber intakekV = new LoggedTunableNumber(
-            "Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kV);
+  private final LoggedTunableNumber intakekP =
+      new LoggedTunableNumber("Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kP);
+  private final LoggedTunableNumber intakekD =
+      new LoggedTunableNumber("Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kD);
+  private final LoggedTunableNumber intakekS =
+      new LoggedTunableNumber("Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kS);
+  private final LoggedTunableNumber intakekV =
+      new LoggedTunableNumber("Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kV);
 
-    private final LoggedTunableNumber deploykP = new LoggedTunableNumber(
-            "Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kP);
-    private final LoggedTunableNumber deploykD = new LoggedTunableNumber(
-            "Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kD);
-    private final LoggedTunableNumber deploykS = new LoggedTunableNumber(
-            "Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kS);
-    private final LoggedTunableNumber deploykV = new LoggedTunableNumber(
-            "Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kV);
-    
-    
+  private final LoggedTunableNumber deploykP =
+      new LoggedTunableNumber("Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kP);
+  private final LoggedTunableNumber deploykD =
+      new LoggedTunableNumber("Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kD);
+  private final LoggedTunableNumber deploykS =
+      new LoggedTunableNumber("Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kS);
+  private final LoggedTunableNumber deploykV =
+      new LoggedTunableNumber("Intake/intake/kP", IntakeConsts.INTAKE_CONFIGS.kV);
 
-
-    public enum intakeState {
-        ON,
-        OFF,
-        TUNING
-    }
+  public enum intakeState {
+    ON,
+    OFF,
+    TUNING
+  }
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -7,44 +7,40 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public class IntakeConstants {
-    public static final class IntakeConsts {
-        public static final int INTAKE_LEAD_ID = 15;
-        public static final int INTAKE_FOLLOWER_ID = 16;
+  public static final class IntakeConsts {
+    public static final int INTAKE_LEAD_ID = 15;
+    public static final int INTAKE_FOLLOWER_ID = 16;
 
-        public static final double TARGET_RPS = 25.0; 
+    public static final double TARGET_RPS = 25.0;
 
-        public static final Slot0Configs INTAKE_CONFIGS = new Slot0Configs()
-                .withKP(0.0)
-                .withKD(0.0)
-                .withKS(0.0)
-                .withKV(0.0);
+    public static final Slot0Configs INTAKE_CONFIGS =
+        new Slot0Configs().withKP(0.0).withKD(0.0).withKS(0.0).withKV(0.0);
 
-        public static final CurrentLimitsConfigs INTAKE_CURRENT_LIMITS_CONFIGS = new CurrentLimitsConfigs()
-                .withStatorCurrentLimit(30.0);
+    public static final CurrentLimitsConfigs INTAKE_CURRENT_LIMITS_CONFIGS =
+        new CurrentLimitsConfigs().withStatorCurrentLimit(30.0);
 
-        public static final MotorOutputConfigs INTAKE_MOTOR_OUTPUT_CONFIGS = new MotorOutputConfigs()
-                .withInverted(InvertedValue.Clockwise_Positive) // check with parker
-                .withNeutralMode(NeutralModeValue.Brake);
+    public static final MotorOutputConfigs INTAKE_MOTOR_OUTPUT_CONFIGS =
+        new MotorOutputConfigs()
+            .withInverted(InvertedValue.Clockwise_Positive) // check with parker
+            .withNeutralMode(NeutralModeValue.Brake);
 
-        public static final MotorOutputConfigs INTAKE_MOTOR_OUTPUT_CONFIGS2 = new MotorOutputConfigs()
-                .withInverted(InvertedValue.CounterClockwise_Positive) // check with parker
-                .withNeutralMode(NeutralModeValue.Brake);
+    public static final MotorOutputConfigs INTAKE_MOTOR_OUTPUT_CONFIGS2 =
+        new MotorOutputConfigs()
+            .withInverted(InvertedValue.CounterClockwise_Positive) // check with parker
+            .withNeutralMode(NeutralModeValue.Brake);
+  }
 
-    }
+  public static final class intakeDeploy {
+    public static final int INTAKE_DEPLOY_ID = 17;
 
-    public static final class intakeDeploy {
-        public static final int INTAKE_DEPLOY_ID = 17;
+    public static final Slot0Configs INTAKE_DEPLOY_SLOT0_CONFIGS =
+        new Slot0Configs().withKP(0.0).withKD(0.0).withKS(0.0).withKV(0.0);
+    public static final CurrentLimitsConfigs INTAKE_DEPLOY_CURRENT_LIMITS_CONFIGS =
+        new CurrentLimitsConfigs().withStatorCurrentLimit(30.0);
 
-        public static final Slot0Configs INTAKE_DEPLOY_SLOT0_CONFIGS = new Slot0Configs()
-                .withKP(0.0)
-                .withKD(0.0)
-                .withKS(0.0)
-                .withKV(0.0);
-        public static final CurrentLimitsConfigs INTAKE_DEPLOY_CURRENT_LIMITS_CONFIGS = new CurrentLimitsConfigs()
-                .withStatorCurrentLimit(30.0);
-
-        public static final MotorOutputConfigs INTAKE_DEPLOY_OUTPUT_CONFIGS = new MotorOutputConfigs()
-        .withInverted(InvertedValue.Clockwise_Positive) // ask parker
-        .withNeutralMode(NeutralModeValue.Brake);
-    }
+    public static final MotorOutputConfigs INTAKE_DEPLOY_OUTPUT_CONFIGS =
+        new MotorOutputConfigs()
+            .withInverted(InvertedValue.Clockwise_Positive) // ask parker
+            .withNeutralMode(NeutralModeValue.Brake);
+  }
 }
